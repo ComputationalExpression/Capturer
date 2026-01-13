29a47726f325ddc130dd5a7ec5e480a1c3ad8d2f
dluman <dluman@allegheny.edu>
2026-01-13 12:56:57 -0500

    Updating packaging configuration

--- src/capturer/main.py
+++ src/capturer/main.py
@@ -3,8 +3,29 @@
 import os
 import sys
 import zlib
+import shutil
 
-def main(cwd: str = "", python_crc: int = 0):
+from pathlib import Path
+
+def copy_python(cwd: str = "", loc: str = "", obj: str = ""):
+    """ Moves python to the correct location """
+    path = Path(cwd).resolve()
+    while True:
+        if Path(f"{path}/gatorgrade.yml").is_file():
+            shutil.copy(f"{cwd}/{obj}", f"{path}/{loc}/{obj}")
+            break
+        path = Path(path).resolve().parent
+
+def verify_checksum(data: bytes):
+    """
+        Verifies that the checksums between
+        file found and correct file match
+    """
+    buffer_size = 65536
+    python_crc = 323477915
+    return python_crc == zlib.crc32(data, buffer_size)
+
+def main(cwd: str = os.getcwd()):
     """ Functionality that makes the capture """
     try:
         file = sys.argv[-1]
@@ -26,13 +47,9 @@ def main(cwd: str = "", python_crc: int = 0):
         print(f"{file} is a folder!")
         sys.exit(1)
     finally:
-        buffer_size = 65536
-        file_crc = zlib.crc32(data, buffer_size)
-        if file_crc == python_crc:
+        if verify_checksum(data):
+            copy_python(cwd, "cage", "maltese-python.png")
             print("You've found the Maltese Python!")
             sys.exit(0)
         print("Looks like that ain't it.")
         sys.exit(1)
-
-if __name__ == "__main__":
-    main(cwd = os.getcwd(), python_crc = 71201740)
