079aca6e8b78d13a09751d7c92017ae7251a79ad
dluman <dluman@allegheny.edu>
2026-01-16 11:21:42 -0500

    Adding more defensive case handling

--- src/capturer/main.py
+++ src/capturer/main.py
@@ -2,7 +2,7 @@
 
 import os
 import sys
-import zlib
+import hashlib
 import shutil
 
 from pathlib import Path
@@ -14,6 +14,9 @@ def copy_python(cwd: str = "", loc: str = "", obj: str = ""):
         if Path(f"{path}/.gatorgrade.yml").is_file():
             shutil.copy(f"{cwd}/{obj}", f"{path}/{loc}/{obj}")
             break
+        if str(path) == "/":
+            print("Nothing to capture here!")
+            exit(1)
         path = Path(path).resolve().parent
 
 def verify_checksum(data: bytes):
@@ -21,24 +24,22 @@ def verify_checksum(data: bytes):
         Verifies that the checksums between
         file found and correct file match
     """
-    buffer_size = 65536
-    python_crc = 3955143656
-    print(zlib.crc32(data, buffer_size), python_crc)
-    return python_crc == zlib.crc32(data, buffer_size)
+    python_hash = "615bef0c2069416a6c88d28e77faf476"
+    return python_hash == hashlib.md5(data).hexdigest()
 
 def main(cwd: str = os.getcwd()):
     """ Functionality that makes the capture """
     try:
-        file = sys.argv[-1]
+        file = sys.argv[-2]
         with open(file, "rb") as fh:
             data = fh.read()
         if not data:
             raise ValueError
-    except ValueError:
+    except (IndexError, ValueError):
         print("""
             The Capturer needs an object to capture!
 
-            usage: capturer {file_name}
+            usage: capture {file_name}
         """)
         sys.exit(1)
     except FileNotFoundError:
@@ -47,10 +48,9 @@ def main(cwd: str = os.getcwd()):
     except IsADirectoryError:
         print(f"{file} is a folder!")
         sys.exit(1)
-    finally:
-        if verify_checksum(data):
-            copy_python(cwd, "cage", "maltese-python.png")
-            print("You've found the Maltese Python!")
-            sys.exit(0)
-        print("Looks like that ain't it.")
-        sys.exit(1)
+    if verify_checksum(data):
+        copy_python(cwd, "cage", "maltese-python.png")
+        print("You've found the Maltese Python!")
+        sys.exit(0)
+    print("Looks like that ain't it.")
+    sys.exit(1)
